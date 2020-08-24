9f776d98d352cad8bd8ae9fb0bce5a1c617eb235
heikipoika <heikipoika@users.noreply.github.com>
2020-08-24 23:29:54 +0200

    Add files via upload
    added some more slot area definitions for test purposes

--- mask_and_colour.m
+++ mask_and_colour.m
@@ -1,16 +1,19 @@
-im_rgb = imread('1.jpg');
+im_rgb = imread('2.jpg');
 
 park_mask = zeros([size(im_rgb(:,:,1)),8]);
 lot_free = zeros(8,1);
 
 %define the mask for each lot 1 to 8
+park_mask(370:400,610:650,6) = 1;
 park_mask(390:410,760:820,7) = 1;
+park_mask(350:390,940:1020,8) = 1;
+%note these could all be manually defined when all slots are empty
 
 im_hsv = rgb2hsv(im_rgb);
 
 for i = 1:8 % for each lot
     temp = abs((im_hsv(:,:,1) .* park_mask(:,:,i)) - 0.2) < 0.05; % is the lot areas hue value 0.2 +/- 0.05 (green tarmac)?
-    lot_free(i) = sum(sum(temp)) > sum(sum(park_mask(:,:,i)))*0.5; % is at least 50% of the lot area green then the lot should be free
+    lot_free(i) = sum(sum(temp)) > sum(sum(park_mask(:,:,i)))*0.4; % is at least 50% of the lot area green then the lot should be free
 end
 
 disp('Number of free lots')
